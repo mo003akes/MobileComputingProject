0590d81fe7f0fe99a8905c2ea91e511cad47fda8
jofa2442 <joey.f3@gmail.com>
2016-03-06 14:46:01 -0400

    Added shopping view

--- app/src/main/java/com/example/colini/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/colini/mobilecomputingproject/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Override //fsajdafhaskl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
