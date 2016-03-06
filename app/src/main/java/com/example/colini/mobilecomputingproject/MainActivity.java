59895d5cdab1ad343451fd181ee36726752af3e1
Yang Yang <yy90axf2010@gmail.com>
2016-03-06 15:49:15 -0400

    I put the Detail view on and Add navi Fragment View

--- app/src/main/java/com/example/colini/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/colini/mobilecomputingproject/MainActivity.java
@@ -1,52 +1,55 @@
 package com.example.colini.mobilecomputingproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    @Override //fsajdafhaskl
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private String[] navi_list;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        listView = (ListView) findViewById(R.id.drawerList);
+        navi_list = getResources().getStringArray(R.array.navi_list);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.on,R.string.on){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
             }
-        });
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,navi_list));
+
+        listView.setOnItemClickListener(this);
+        //getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        System.out.println(navi_list[position]);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (navi_list[position].equalsIgnoreCase("Detail")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainContainer,new View_Detial()).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/colini/mobilecomputingproject/View_Detial.java
@@ -0,0 +1,21 @@
+package com.example.colini.mobilecomputingproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yang on 16-03-06.
+ */
+public class View_Detial extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.view_detail,container,false);
+        return rootView;
+    }
+}
